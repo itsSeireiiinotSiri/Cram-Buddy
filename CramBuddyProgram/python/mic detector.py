52d819a5142911321cb22c0489db3de0257b83dc
Ricktheboi07 <46830933+itsSeireiiinotSiri@users.noreply.github.com>
2023-06-06 11:13:30 -0400

    API core removal, preparation to add new one

--- CramBuddyProgram/python/mic detector.py
+++
@@ -1,9 +0,0 @@
-import pyaudio
-
-pa = pyaudio.PyAudio()
-info = pa.get_host_api_info_by_index(0)
-num_devices = info.get('deviceCount')
-
-for i in range(num_devices):
-    device_info = pa.get_device_info_by_host_api_device_index(0, i)
-    print(f"Device {i}: {device_info['name']}")
